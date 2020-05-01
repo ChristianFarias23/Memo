f8d3554a85936698fc57085b8e34d03cd713631f
Christian Farias <kurisu615@gmail.com>
2020-05-01 05:14:04 -0400

    Add efecto DropShadow.cs

---
+++ Memo/Assets/Scripts/Effect/DropShadow.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Memo.Shaders
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class DropShadow : MonoBehaviour
+    {
+        public Vector2 ShadowOffset;
+        public Material ShadowMaterial;
+
+        SpriteRenderer spriteRenderer;
+        GameObject shadowGameobject;
+
+        void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+            //create a new gameobject to be used as drop shadow
+            shadowGameobject = new GameObject("Shadow");
+            shadowGameobject.transform.parent = transform;
+
+            //create a new SpriteRenderer for Shadow gameobject
+            SpriteRenderer shadowSpriteRenderer = shadowGameobject.AddComponent<SpriteRenderer>();
+
+            //set the shadow gameobject's sprite to the original sprite
+            shadowSpriteRenderer.sprite = spriteRenderer.sprite;
+            //set the shadow gameobject's material to the shadow material we created
+            shadowSpriteRenderer.material = ShadowMaterial;
+            shadowSpriteRenderer.drawMode = spriteRenderer.drawMode;
+            shadowSpriteRenderer.size = spriteRenderer.size;
+
+            //update the sorting layer of the shadow to always lie behind the sprite
+            shadowSpriteRenderer.sortingLayerName = spriteRenderer.sortingLayerName;
+            shadowSpriteRenderer.sortingOrder = spriteRenderer.sortingOrder - 1;
+        }
+
+        void LateUpdate()
+        {
+            //update the position and rotation of the sprite's shadow with moving sprite
+            shadowGameobject.transform.position = transform.localPosition + (Vector3)ShadowOffset;
+            shadowGameobject.transform.rotation = transform.localRotation;
+        }
+    }
+}
